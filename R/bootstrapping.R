429f8a17a97039b981362edfa9c80ddc9278b396
Sanchez Alvarez, Javier {MGPF~Basel} <javier.sanchez_alvarez@roche.com>
2022-09-14 08:31:03 +0200

    -Addition of extra functions

--- R/bootstrapping.R
+++ R/bootstrapping.R
@@ -109,3 +109,163 @@ bootstrap_OR <- function(intervention_data, matching, i, model, comparator_data,
   logistic.regr <- suppressWarnings(stats::glm(formula = model, family=stats::binomial(link="logit"), data = combined_data, weight = wt))
   OR <- exp(as.numeric(stats::coef(logistic.regr)[2]))
 }
+
+#' Bootstrapping for MAIC weighted  difference in means
+#'
+#' @param intervention_data  A data frame containing individual patient data
+#'   from the intervention study.
+#' @param matching A character vector giving the names of the covariates to use
+#'   in matching. These names must match the column names in intervention_data.
+#' @param i Index used to select a sample within \code{\link{boot}}.
+#' @param comparator_data A data frame containing pseudo individual patient data
+#'   from the comparator study needed to derive the relative treatment effect.
+#'   The outcome variables names must match intervention_data.
+#' @param contvarname a character value that defines the variable name of the continuous variable to be analyzed
+#' @param min_weight A numeric value that defines the minimum weight allowed. 
+#'   This value (default 0.0001) will replace weights estimated at 0 in a sample.
+#'
+#' @details This function is intended to be used in conjunction with the
+#'   \code{\link{boot}} function to return the statistic to be
+#'   bootstrapped. In this case by performing MAIC weighting using
+#'   {\link{estimate_weights}} and returning a weighted difference in means.
+#'   This is used as the 'statistic' argument in
+#'   the boot function.
+#'
+#' @return The weighted mean as a numeric value.
+#'
+#' @seealso \code{\link{estimate_weights}}, \code{\link{boot}}
+#'
+#' @example inst/examples/MAIC_example_analysis.R
+#'
+#' @export
+bootstrap_means <- function(intervention_data, matching, i, comparator_data, contvarname,
+                           min_weight = 0.0001){
+  # create a visible binding for R CMD check
+  wt <- NULL
+  
+  
+  bootstrap_data <- intervention_data[i, ]
+  perform_wt <- estimate_weights(intervention_data = bootstrap_data, 
+                                 matching_vars = matching)
+  
+  # Give comparator data weights of 1
+  comparator_data_wts <- comparator_data %>% dplyr::mutate(wt=1, wt_rs=1, ARM="Comparator")
+  
+  # Add the comparator data
+  intervention_data_wts <- perform_wt$analysis_data
+  
+  # Exclude NA values for contvarname
+  intervention_data_wts <- intervention_data_wts[!is.na(intervention_data_wts[,contvarname]),]
+  
+  combined_data <- dplyr::bind_rows(intervention_data_wts, comparator_data_wts)
+  combined_data$ARM <- stats::relevel(as.factor(combined_data$ARM), ref="Comparator")
+  combined_data$wt <- ifelse(combined_data$wt < min_weight, 
+                             min_weight, combined_data$wt)
+  
+  mn <- sum(combined_data[,"wt"] * combined_data[,contvarname])/sum(combined_data[,"wt"])
+}
+
+#' Bootstrapping for MAIC weighted rate ratios (RR)
+#'
+#' @param intervention_data  A data frame containing individual patient data
+#'   from the intervention study.
+#' @param matching A character vector giving the names of the covariates to use
+#'   in matching. These names must match the column names in intervention_data.
+#' @param i Index used to select a sample within \code{\link{boot}}.
+#' @param eventsvar Character variable name for number of events
+#' @param expvar Character variable name for exposure
+#' @param comparator_data A data frame containing pseudo individual patient data
+#'   from the comparator study needed to derive the relative treatment effect.
+#'   The outcome variables names must match intervention_data.
+#' @param min_weight A numeric value that defines the minimum weight allowed. 
+#'   This value (default 0.0001) will replace weights estimated at 0 in a sample.
+#'
+#' @details This function is intended to be used in conjunction with the
+#'   \code{\link{boot}} function to return the statistic to be
+#'   bootstrapped. In this case by performing MAIC weighting using
+#'   {\link{estimate_weights}} and returning a weighted rates ratio.
+#'   This is used as the 'statistic' argument in
+#'   the boot function.
+#'
+#' @return The rate as a numeric value.
+#'
+#' @seealso \code{\link{estimate_weights}}, \code{\link{boot}}
+#'
+#' @example inst/examples/MAIC_example_analysis.R
+#'
+#' @export
+bootstrap_rate <- function(intervention_data, matching, i, comparator_data, eventsvar, expvar,
+                           min_weight = 0.0001){
+  
+  bootstrap_data <- intervention_data[i, ]
+  perform_wt <- estimate_weights(intervention_data = bootstrap_data, 
+                                 matching_vars = matching)
+  
+  intervention_data_wts <- perform_wt$analysis_data
+  
+  comparator_data_wts <- comparator_data %>% dplyr::mutate(wt=1, wt_rs=1, ARM="Comparator")
+  
+  combined_data <- dplyr::bind_rows(intervention_data_wts, comparator_data_wts)
+  combined_data$ARM <- stats::relevel(as.factor(combined_data$ARM), ref="Comparator")
+  combined_data$wt <- ifelse(combined_data$wt < min_weight, 
+                             min_weight, combined_data$wt)
+    
+  events <- sum(combined_data[,eventsvar]*combined_data[,"wt"])
+  exposure <- sum(combined_data[,expvar]*combined_data[,"wt"])
+  
+  rate <- events/exposure 
+}
+
+
+#' Bootstrapping for MAIC weighted incidence rate from neg binom model to feed into rate ratios
+#'
+#' @param intervention_data  A data frame containing individual patient data
+#'   from the intervention study.
+#' @param matching A character vector giving the names of the covariates to use
+#'   in matching. These names must match the column names in intervention_data.
+#' @param i Index used to select a sample within \code{\link{boot}}.
+#' @param comparator_data A data frame containing pseudo individual patient data
+#'   from the comparator study needed to derive the relative treatment effect.
+#'   The outcome variables names must match intervention_data.   
+#' @param formula formula for the negative binomial regression
+#' @param min_weight A numeric value that defines the minimum weight allowed. 
+#'   This value (default 0.0001) will replace weights estimated at 0 in a sample.
+#'
+#' @details This function is intended to be used in conjunction with the
+#'   \code{\link{boot}} function to return the statistic to be
+#'   bootstrapped. In this case by performing MAIC weighting using
+#'   {\link{estimate_weights}} and returning a weighted rates ratio.
+#'   This is used as the 'statistic' argument in
+#'   the boot function.
+#'
+#' @return The IR as a numeric value.
+#'
+#' @seealso \code{\link{estimate_weights}}, \code{\link{boot}}
+#'
+#' @example inst/examples/MAIC_example_analysis.R
+#'
+#' @export
+bootstrap_ir <- function(intervention_data, matching, i, comparator_data, formula,
+                           min_weight = 0.0001){
+  
+  bootstrap_data <- intervention_data[i,]
+  # Estimates weights
+  perform_wt <- estimate_weights(intervention_data=bootstrap_data, matching_vars=matching)
+  
+  intervention_data_wts <- perform_wt$analysis_data
+  
+  comparator_data_wts <- comparator_data %>% dplyr::mutate(wt=1, wt_rs=1, ARM="Comparator")
+  
+  combined_data <- dplyr::bind_rows(intervention_data_wts, comparator_data_wts)
+  combined_data$ARM <- stats::relevel(as.factor(combined_data$ARM), ref="Comparator")
+  
+  combined_data$wt <- ifelse(combined_data$wt < min_weight, 
+                                           min_weight, combined_data$wt)
+  
+  nbmod_wt_boot <- MASS::glm.nb(as.formula(formula), data=combined_data, weights=wt)
+  
+  lsm_wt_boot <- emmeans::emmeans(nbmod_wt_boot, "ARM", offset=0, options=list(tran="log"), type="response", data=combined_data)
+  
+  IR <- summary(lsm_wt_boot)$response[summary(lsm_wt_boot)$ARM!="Comparator"]
+  
+}
--- inst/examples/MAIC_example_analysis.R
+++ inst/examples/MAIC_example_analysis.R
@@ -16,6 +16,13 @@ library(officer)
 # load intervention data with weights saved in est_weights
 data(est_weights, package = "MAIC")
 
+intervention_mean <- 11
+intervention_sd <- 4
+est_weights$analysis_data$diff_from_baseline <- rnorm(nrow(est_weights$analysis_data),intervention_mean,intervention_sd)
+
+est_weights$analysis_data$exp <- est_weights$analysis_data$Time
+
+
 # Combine data -----------------------------------------------------------------
 
 # Combine the the comparator pseudo data with the analysis data, outputted from
@@ -37,13 +44,22 @@ comparator_prop_events <- 0.4 # proportion of responders
 n_with_event <- round(comparator_n*comparator_prop_events, digits = 0)
 comparator_binary <- data.frame("response"= c(rep(1, n_with_event), rep(0, comparator_n - n_with_event)))
 
+comparator_mean <- 10
+comparator_sd <- 4
+comparator_cont <- data.frame("diff_from_baseline"= rnorm(comparator_n,comparator_mean,comparator_sd))
+
+comparator_exp <- rexp(comparator_n,0.000018)
+comparator_events <- n_with_event
+comparator_exposure <- data.frame("exp"= comparator_exp)
+
+
 
 # Join survival and response comparator data
 # Note the rows do not represent observations from a particular patient
 # i.e. there is no relationship between the survival time and response status
 # for a given row since this is simulated data
 # In a real data set this relationship would be present
-comparator_input <- cbind(comparator_surv, comparator_binary) %>%
+comparator_input <- cbind(comparator_surv, comparator_binary, comparator_cont,comparator_exposure) %>%
   mutate(wt=1, wt_rs=1, ARM="Comparator") # All patients have weight = 1
 head(comparator_input)
 
@@ -273,3 +289,236 @@ OR_table <- OR_summ %>%
             border_inner_h(border = fp_border()) %>%
             border_inner_v(border = fp_border()) %>%
             autofit(add_w = 0.2)
+
+### Example for continuous data --------------------------------------------------
+
+comparator_n <- comparator_n
+comparator_mean <-comparator_mean
+comparator_sd <- comparator_sd
+comparator_se <- comparator_sd/sqrt(comparator_n)
+comparator_lcl <- comparator_mean - qnorm(0.975)*comparator_se
+comparator_ucl <- comparator_mean + qnorm(0.975)*comparator_se
+comparator_summary <- paste0(round(comparator_mean, 2), " (", round(comparator_lcl, 2), ", ", round(comparator_ucl, 2), ")")
+
+
+intervention_n <- nrow(est_weights$analysis_data)
+intervention_mean <- mean(est_weights$analysis_data[,"diff_from_baseline"])
+intervention_sd <- sd(est_weights$analysis_data[,"diff_from_baseline"])
+intervention_se <- intervention_sd/sqrt(intervention_n)
+intervention_lcl <- intervention_mean - qnorm(0.975)*intervention_se
+intervention_ucl <- intervention_mean + qnorm(0.975)*intervention_se
+intervention_summary <- paste0(round(intervention_mean, 2), " (", round(intervention_lcl, 2), ", ", round(intervention_ucl, 2), ")")
+
+
+intervention_n_w <- sum(est_weights$analysis_data[,"wt"])
+intervention_mean_w <-     sum(est_weights$analysis_data[,"wt"] * est_weights$analysis_data[,"diff_from_baseline"])/sum(est_weights$analysis_data[,"wt"])
+intervention_sd_w <- sqrt(Hmisc::wtd.var(est_weights$analysis_data[,"diff_from_baseline"], w=est_weights$analysis_data[,"wt"])) 
+intervention_se_w <- intervention_sd_w/sqrt(intervention_n_w)
+intervention_lcl_w <- intervention_mean_w - qnorm(0.975)*intervention_se_w
+intervention_ucl_w <- intervention_mean_w + qnorm(0.975)*intervention_se_w
+intervention_summary_w <- paste0(round(intervention_mean_w, 2), " (", round(intervention_lcl_w, 2), ", ", round(intervention_ucl_w, 2), ")")
+
+
+## Compare arms
+
+diff_u <- intervention_mean - comparator_mean
+se_diff_u <- sqrt(intervention_se^2 + comparator_se^2)
+lcl_diff_u <- diff_u-qnorm(0.975)*se_diff_u
+ucl_diff_u <- diff_u+qnorm(0.975)*se_diff_u
+pval_u <- 2*min(pnorm(as.numeric(diff_u/se_diff_u)), 1-pnorm(as.numeric(diff_u/se_diff_u)))
+
+diff_w <- intervention_mean_w - comparator_mean
+se_diff_w <- sqrt(intervention_se_w^2 + comparator_se^2)
+lcl_diff_w <- diff_w-qnorm(0.975)*se_diff_w
+ucl_diff_w <- diff_w+qnorm(0.975)*se_diff_w
+pval_w <- 2*min(pnorm(as.numeric(diff_w/se_diff_w)), 1-pnorm(as.numeric(diff_w/se_diff_w)))
+
+
+## Bootstrapping mean in intervention arm
+mean_bootstraps <- boot(data = est_weights$analysis_data,
+                        comparator_data = combined_data%>% filter(ARM=="Comparator"), 
+                        statistic = bootstrap_means, # bootstrap the exp arm mean
+                        R = 1000, # number of bootstrap samples
+                        matching = est_weights$matching_vars, # matching variables
+                        contvarname="diff_from_baseline"
+)
+
+# Bootstrap standard error of mean in intervention arm and use it to calculate bootstrap CI and p-value for difference
+
+mean_bootvar <- var(mean_bootstraps$t)
+
+se_diff_w_boot <- sqrt(mean_bootvar + comparator_se^2)
+lcl_diff_w_boot <- diff_w-qnorm(0.975)*se_diff_w_boot
+ucl_diff_w_boot <- diff_w+qnorm(0.975)*se_diff_w_boot
+pval_w_boot <- 2*min(pnorm(as.numeric(diff_w/se_diff_w_boot)), 1-pnorm(as.numeric(diff_w/se_diff_w_boot)))
+
+
+## Summarise and tabulate
+
+diff_summary_standard   <- paste0(round(diff_u,2), " (", round(lcl_diff_u,2), ", ", round(ucl_diff_u, 2), ")")
+diff_summary_standard_w <- paste0(round(diff_w,2), " (", round(lcl_diff_w,2), ", ", round(ucl_diff_w, 2), ")")
+
+diff_summary_boot_w <- paste0(round(diff_w,2), " (", round(lcl_diff_w_boot,2), ", ", round(ucl_diff_w_boot, 2), ")")
+
+
+
+## Example for rates data ----------------------------------------------------------------------------------------------------------------------------------
+
+
+comparator_events <- comparator_events
+comparator_exp <- flexsurv::mean_exp(0.00005)
+comparator_n <- comparator_n
+comparator_rate <- comparator_events/comparator_exp
+comparator_summary <- paste0(round(comparator_rate, 4), " (", comparator_events, "/", round(comparator_exp,1), ")")
+
+intervention_events <- sum(est_weights$analysis_data[,"response"])
+intervention_exp <- sum(est_weights$analysis_data[,"exp"])
+intervention_n <- nrow(est_weights$analysis_data)
+intervention_rate <- intervention_events/intervention_exp
+intervention_summary <- paste0(round(intervention_rate, 4), " (", intervention_events, "/", round(intervention_exp,1), ")")
+
+intervention_events_w <- sum(est_weights$analysis_data[,"response"]*est_weights$analysis_data[,"wt"])
+intervention_exp_w <- sum(est_weights$analysis_data[,"exp"]*est_weights$analysis_data[,"wt"])
+intervention_n_w <- sum(est_weights$analysis_data[,"wt"])
+intervention_rate_w <- intervention_events_w/intervention_exp_w
+intervention_summary_w <- paste0(round(intervention_rate_w, 4), " (", round(intervention_events_w,1), "/", round(intervention_exp_w,1), ")")
+
+
+## Compare arms
+
+rr_u <- intervention_rate / comparator_rate
+lrr_u <- log(rr_u)
+se_lrr_u <- sqrt(1/intervention_events + 1/comparator_events)
+lcl_rr_u <- exp(lrr_u-qnorm(0.975)*se_lrr_u)
+ucl_rr_u <- exp(lrr_u+qnorm(0.975)*se_lrr_u)
+pval_u <- 2*min(pnorm(as.numeric(lrr_u/se_lrr_u)), 1-pnorm(as.numeric(lrr_u/se_lrr_u)))
+
+rr_w <- intervention_rate_w / comparator_rate
+lrr_w <- log(rr_w)
+se_lrr_w <- sqrt(1/intervention_events_w + 1/comparator_events)
+lcl_rr_w <- exp(lrr_w-qnorm(0.975)*se_lrr_w)
+ucl_rr_w <- exp(lrr_w+qnorm(0.975)*se_lrr_w)
+pval_w <- 2*min(pnorm(as.numeric(lrr_w/se_lrr_w)), 1-pnorm(as.numeric(lrr_w/se_lrr_w)))
+
+
+## Bootstrapping rate in intervention arm
+
+
+rate_bootstraps <- boot(data = est_weights$analysis_data, 
+                        statistic = bootstrap_rate, # bootstrap the exp arm rate
+                        R = 1000, # number of bootstrap samples
+                        comparator_data = combined_data%>% filter(ARM=="Comparator"), 
+                        matching = est_weights$matching_vars, # matching variables
+                        eventsvar="response",
+                        expvar="exp"
+)
+
+# Bootstrap standard error of log rate in intervention arm and use it to calculate bootstrap CI and p-value for RR
+
+lograte_bootvar <- var(log(rate_bootstraps$t))
+
+se_lrr_w_boot <- sqrt(lograte_bootvar + 1/comparator_events)
+lcl_rr_w_boot <- exp(lrr_w-qnorm(0.975)*se_lrr_w_boot)
+ucl_rr_w_boot <- exp(lrr_w+qnorm(0.975)*se_lrr_w_boot)
+pval_w_boot <- 2*min(pnorm(as.numeric(lrr_w/se_lrr_w_boot)), 1-pnorm(as.numeric(lrr_w/se_lrr_w_boot)))
+
+## Summarise and tabulate
+
+rr_summary_standard   <- paste0(round(rr_u,3), " (", round(lcl_rr_u,3), ", ", round(ucl_rr_u, 3), ")")
+rr_summary_standard_w <- paste0(round(rr_w,3), " (", round(lcl_rr_w,3), ", ", round(ucl_rr_w, 3), ")")
+rr_summary_boot_w <- paste0(round(rr_w,3), " (", round(lcl_rr_w_boot,3), ", ", round(ucl_rr_w_boot, 3), ")")
+
+
+
+
+# Example for incidence rate to rr ------------------------------------------------------------------------------------------------------------------------
+
+comparator_arr <- comparator_rate
+comparator_n <- comparator_n
+comparator_dur_yrs <- comparator_exp/comparator_n/365.25
+comparator_durtot <- comparator_dur_yrs*comparator_n
+comparator_etot <- comparator_n * (comparator_exp/365.25)/(comparator_n*0.9)
+comparator_rtot_from_dur <- comparator_arr*comparator_durtot
+comparator_rtot_from_etot <- comparator_arr*comparator_etot
+comparator_summary_dur <- paste0(round(comparator_arr, 3), " (", round(comparator_rtot_from_dur, 1), "/", round(comparator_durtot, 1), ")")
+comparator_summary_etot <- paste0(round(comparator_arr, 3), " (", round(comparator_rtot_from_etot, 1), "/", round(comparator_etot, 1), ")")
+
+intervention_arr <- intervention_rate
+intervention_n <- nrow(est_weights$analysis_data)
+intervention_dur_yrs <- max(est_weights$analysis_data$Time)
+intervention_durtot <- intervention_dur_yrs*intervention_n
+intervention_etot <- sum(est_weights$analysis_data$exp)
+intervention_rtot_from_dur <- intervention_arr*intervention_durtot
+intervention_rtot_from_etot <- intervention_arr*intervention_etot
+intervention_summary_dur <- paste0(round(intervention_arr, 3), " (", round(intervention_rtot_from_dur, 1), "/", round(intervention_durtot, 1), ")")
+intervention_summary_etot <- paste0(round(intervention_arr, 3), " (", round(intervention_rtot_from_etot, 1), "/", round(intervention_etot, 1), ")")
+
+intervention_arr_w <- intervention_rate_w
+intervention_n_w <- intervention_n_w
+intervention_dur_yrs_w <- max(est_weights$analysis_data$Time*est_weights$analysis_data$wt)
+intervention_durtot_w <- intervention_dur_yrs_w*intervention_n_w
+intervention_etot_w <- sum(est_weights$analysis_data$exp * est_weights$analysis_data$wt)
+intervention_rtot_from_dur_w <- intervention_arr_w*intervention_durtot_w
+intervention_rtot_from_etot_w <- intervention_arr_w*intervention_etot_w
+intervention_summary_dur_w <- paste0(round(intervention_arr_w, 3), " (", round(intervention_rtot_from_dur_w, 1), "/", round(intervention_durtot_w, 1), ")")
+intervention_summary_etot_w <- paste0(round(intervention_arr_w, 3), " (", round(intervention_rtot_from_etot_w, 1), "/", round(intervention_etot_w, 1), ")")
+
+## Compare arms
+
+rr_u <- intervention_arr / comparator_arr
+lrr_u <- log(rr_u)
+se_lrr_dur_u <- sqrt(1/intervention_rtot_from_dur + 1/comparator_rtot_from_dur)
+lcl_rr_dur_u <- exp(lrr_u-qnorm(0.975)*se_lrr_dur_u)
+ucl_rr_dur_u <- exp(lrr_u+qnorm(0.975)*se_lrr_dur_u)
+se_lrr_etot_u <- sqrt(1/intervention_rtot_from_etot + 1/comparator_rtot_from_etot)
+lcl_rr_etot_u <- exp(lrr_u-qnorm(0.975)*se_lrr_etot_u)
+ucl_rr_etot_u <- exp(lrr_u+qnorm(0.975)*se_lrr_etot_u)
+pval_dur_u <- 2*min(pnorm(as.numeric(lrr_u/se_lrr_dur_u)), 1-pnorm(as.numeric(lrr_u/se_lrr_dur_u)))
+pval_etot_u <- 2*min(pnorm(as.numeric(lrr_u/se_lrr_etot_u)), 1-pnorm(as.numeric(lrr_u/se_lrr_etot_u)))
+
+rr_w <- intervention_arr_w / comparator_arr
+lrr_w <- log(rr_w)
+se_lrr_dur_w <- sqrt(1/intervention_rtot_from_dur_w + 1/comparator_rtot_from_dur)
+lcl_rr_dur_w <- exp(lrr_w-qnorm(0.975)*se_lrr_dur_w)
+ucl_rr_dur_w <- exp(lrr_w+qnorm(0.975)*se_lrr_dur_w)
+se_lrr_etot_w <- sqrt(1/intervention_rtot_from_etot_w + 1/comparator_rtot_from_etot)
+lcl_rr_etot_w <- exp(lrr_w-qnorm(0.975)*se_lrr_etot_w)
+ucl_rr_etot_w <- exp(lrr_w+qnorm(0.975)*se_lrr_etot_w)
+pval_dur_w <- 2*min(pnorm(as.numeric(lrr_w/se_lrr_dur_w)), 1-pnorm(as.numeric(lrr_w/se_lrr_dur_w)))
+pval_etot_w <- 2*min(pnorm(as.numeric(lrr_w/se_lrr_etot_w)), 1-pnorm(as.numeric(lrr_w/se_lrr_etot_w)))
+
+
+## Bootstrapping incidence rate in intervention arm
+
+IR_bootstraps <- boot(data = est_weights$analysis_data, 
+                      statistic = bootstrap_ir, # bootstrap the exp arm incidence rate
+                      R = 1000, # number of bootstrap samples
+                      comparator_data = combined_data%>% filter(ARM=="Comparator"), 
+                      formula = "response~ARM+offset(log(exp))",
+                      matching = est_weights$matching_vars # matching variables
+)
+
+# Bootstrap standard error of log IR and calculate RR CI and p-value
+IR_bootvar <- var(log(IR_bootstraps$t))
+
+se_lrr_dur_w_boot <- sqrt(IR_bootvar + 1/comparator_rtot_from_dur)
+lcl_rr_dur_w_boot <- exp(lrr_w-qnorm(0.975)*se_lrr_dur_w_boot)
+ucl_rr_dur_w_boot <- exp(lrr_w+qnorm(0.975)*se_lrr_dur_w_boot)
+se_lrr_etot_w_boot <- sqrt(IR_bootvar + 1/comparator_rtot_from_etot)
+lcl_rr_etot_w_boot <- exp(lrr_w-qnorm(0.975)*se_lrr_etot_w_boot)
+ucl_rr_etot_w_boot <- exp(lrr_w+qnorm(0.975)*se_lrr_etot_w_boot)
+pval_dur_w_boot <- 2*min(pnorm(as.numeric(lrr_w/se_lrr_dur_w_boot)), 1-pnorm(as.numeric(lrr_w/se_lrr_dur_w_boot)))
+pval_etot_w_boot <- 2*min(pnorm(as.numeric(lrr_w/se_lrr_etot_w_boot)), 1-pnorm(as.numeric(lrr_w/se_lrr_etot_w_boot)))
+
+
+# Summarise and tabulate
+
+rr_summary_standard   <- paste0(round(rr_u,3), " (", round(lcl_rr_dur_u,3), ", ", round(ucl_rr_dur_u, 3), ")")
+rr_summary_standard_w <- paste0(round(rr_w,3), " (", round(lcl_rr_dur_w,3), ", ", round(ucl_rr_dur_w, 3), ")")
+
+rr_summary_standard_etot   <- paste0(round(rr_u,3), " (", round(lcl_rr_etot_u,3), ", ", round(ucl_rr_etot_u, 3), ")")
+rr_summary_standard_etot_w <- paste0(round(rr_w,3), " (", round(lcl_rr_etot_w,3), ", ", round(ucl_rr_etot_w, 3), ")")
+
+rr_summary_boot_w <- paste0(round(rr_w,3), " (", round(lcl_rr_dur_w_boot,3), ", ", round(ucl_rr_dur_w_boot, 3), ")")
+rr_summary_boot_etot_w <- paste0(round(rr_w,3), " (", round(lcl_rr_etot_w_boot,3), ", ", round(ucl_rr_etot_w_boot, 3), ")")
+
